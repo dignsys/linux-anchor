a6dff54861f53c3730824709c1f1580cf45e43b7
Hyobok Ahn <hbahn@dignsys.com>
2021-02-13 15:35:27 +0900

    support new LCD panel, avd-tt80wx-056-A

--- drivers/gpu/drm/panel/panel-avdtt80wx.c
+++ drivers/gpu/drm/panel/panel-avdtt80wx.c
@@ -26,13 +26,35 @@
 #define H_ACTIVE     		800
 #define V_ACTIVE     		1280
 
-#define H_FRONT_PORCH		(10+100)
-#define H_BACK_PORCH		(30+100)
+#ifdef CONFIG_DRM_PANEL_AVDTT80WXCN046A
+#define H_FRONT_PORCH		(110)
+#define H_BACK_PORCH		(130)
 #define HS_LOW_PULSE_WIDTH	(8)
 
-#define V_FRONT_PORCH		(2+10)
-#define V_BACK_PORCH		(2+5)
-#define VS_LOW_PULSE_WIDTH	(2+1)
+#define V_FRONT_PORCH		(12)
+#define V_BACK_PORCH		(7)
+#define VS_LOW_PULSE_WIDTH	(3)
+#endif /* CONFIG_DRM_PANEL_AVDTT80WXCN046A */
+
+#ifdef CONFIG_DRM_PANEL_AVDTT80WXCN039A
+#define H_FRONT_PORCH           (60)
+#define H_BACK_PORCH            (60)
+#define HS_LOW_PULSE_WIDTH      (40)
+
+#define V_FRONT_PORCH           (16)
+#define V_BACK_PORCH            (16)
+#define VS_LOW_PULSE_WIDTH      (8)
+#endif /* CONFIG_DRM_PANEL_AVDTT80WXCN039A */
+
+#ifdef CONFIG_DRM_PANEL_AVDTT80WXCN056A
+#define H_FRONT_PORCH           (100)
+#define H_BACK_PORCH            (100)
+#define HS_LOW_PULSE_WIDTH      (10)
+
+#define V_FRONT_PORCH           (16)
+#define V_BACK_PORCH            (16)
+#define VS_LOW_PULSE_WIDTH      (8)
+#endif /* CONFIG_DRM_PANEL_AVDTT80WXCN056A */
 
 #define VREFRESH		60
 #define WIDTH_MM		107
@@ -43,13 +65,14 @@
 #define SLPIN			0x10
 #define SLPOUT			0x11
 #define DISPON			0x29
+#define TEON			0x35
 #define DCSGETID1		0xDA
 #define DCSGETID2		0xDB
 #define DCSGETID3		0xDC
 
 #define MODULEID1		0x83
-#define MODULEID2       0x94
-#define MODULEID3       0x0D
+#define MODULEID2		0x94
+#define MODULEID3		0x0D
 
 #define MAX_BACKLIGHT_BRIGHTNESS 175
 #define MIN_BACKLIGHT_BRIGHTNESS 80
@@ -166,10 +189,10 @@ static int __maybe_unused _dcs_read(struct avdtt80wx *ctx, u8 cmd, void *data, s
 
 static void _set_sequence(struct avdtt80wx *ctx)
 {
+#ifdef CONFIG_DRM_PANEL_AVDTT80WXCN046A
 	/* Datasheet: AVD-TT80WX-CN-046-A.pdf */
-
 	static u8 d[193][8] = {
-		{4,	0xFF, 0x98, 0x81, 0x03},
+		{4,	0xFF, 98, 81, 03},
 		{2,	0x01, 0x00},
 		{2,	0x02, 0x00},
 		{2,	0x03, 0x53},
@@ -297,7 +320,7 @@ static void _set_sequence(struct avdtt80wx *ctx)
 		{2,	0x88, 0x02},    
 		{2,	0x89, 0x02},      
 		{2,	0x8A, 0x02},  
-		{4,	0xFF, 0x98, 0x81, 0x04}, 
+		{4,	0xFF, 98, 81, 04}, 
 		{2,	0x6C, 0x15},
 		{2,	0x6E, 0x30}, 
 		{2,	0x6F, 0x33}, 
@@ -311,7 +334,7 @@ static void _set_sequence(struct avdtt80wx *ctx)
 		{2,	0x3B, 0x98},
 		{2,	0x38, 0x01},
 		{2,	0x39, 0x00},
-		{4,	0xFF, 0x98, 0x81, 0x01}, //CMD_Page 1
+		{4,	0xFF, 98, 81, 01}, //CMD_Page 1
 		{2,	0x22, 0x0A},
 		{2,	0x31, 0x00},   
 		{2,	0x50, 0xC0},		
@@ -360,9 +383,442 @@ static void _set_sequence(struct avdtt80wx *ctx)
 		{2,	0xD1, 0x4E},         
 		{2,	0xD2, 0x5F},                  
 		{2,	0xD3, 0x39}, 
-		{4,	0xFF, 0x98, 0x81, 0x00}, 
+		{4,	0xFF, 98, 81, 00}, 
 		{2,	0x35, 0x00}
 	};
+#endif /* CONFIG_DRM_PANEL_AVDTT80WXCN046A */
+
+#ifdef CONFIG_DRM_PANEL_AVDTT80WXCN039A
+	/* Datasheet: AVD-TT80WX-CN-039-A.pdf */
+	static u8 d[196][8] = {
+		{4 , 0xFF , 0x98 , 0x81 , 0x03},
+
+		//GIP_1
+		{2 , 0x01 , 0x00},
+		{2 , 0x02 , 0x00},
+		{2 , 0x03 , 0x73},     //STA Width 4H
+		{2 , 0x04 , 0x00},
+		{2 , 0x05 , 0x00},
+		{2 , 0x06 , 0x0A},    //STVA Rise start
+		{2 , 0x07 , 0x00},
+		{2 , 0x08 , 0x00},
+		{2 , 0x09 , 0x20},    //Detail A&B 3.5-4.0us
+		{2 , 0x0A , 0x20},   //Detail A&B 3.5-4.0us
+		{2 , 0x0B , 0x00},
+		{2 , 0x0C , 0x00},
+		{2 , 0x0D , 0x00},
+		{2 , 0x0E , 0x00},
+		{2 , 0x0F , 0x1E},    //Detail A&B 3.5-4.0us
+		{2 , 0x10 , 0x1E},    //Detail A&B 3.5-4.0us 
+		{2 , 0x11 , 0x00},
+		{2 , 0x12 , 0x00},
+		{2 , 0x13 , 0x00},
+		{2 , 0x14 , 0x00},
+		{2 , 0x15 , 0x10},/////////////
+		{2 , 0x16 , 0x10},//////
+		{2 , 0x17 , 0x03},
+		{2 , 0x18 , 0x03},
+		{2 , 0x19 , 0x00},
+		{2 , 0x1A , 0x00},
+		{2 , 0x1B , 0x00},
+		{2 , 0x1C , 0x00},
+		{2 , 0x1D , 0x00},
+		{2 , 0x1E , 0x40},
+		{2 , 0x1F , 0x80},
+		{2 , 0x20 , 0x06},   //CLKA Rise START
+		{2 , 0x21 , 0x01},   //CLKA FALL END
+		{2 , 0x22 , 0x00},
+		{2 , 0x23 , 0x00},
+		{2 , 0x24 , 0x00},
+		{2 , 0x25 , 0x00},
+		{2 , 0x26 , 0x00},
+		{2 , 0x27 , 0x00},
+		{2 , 0x28 , 0x33},   //CLK_x_Numb[2:0]  Phase_CLK[2:0]
+		{2 , 0x29 , 0x03},   //Overlap_CLK[3:0]
+		{2 , 0x2A , 0x00},
+		{2 , 0x2B , 0x00},
+		{2 , 0x2C , 0x00},
+		{2 , 0x2D , 0x00},
+		{2 , 0x2E , 0x00},
+		{2 , 0x2F , 0x00},
+		{2 , 0x30 , 0x00},
+		{2 , 0x31 , 0x00},
+		{2 , 0x32 , 0x00},
+		{2 , 0x33 , 0x00},
+		{2 , 0x34 , 0x04},    //04 GPWR1/2 non overlap time 2.62us
+		{2 , 0x35 , 0x00},
+		{2 , 0x36 , 0x00},
+		{2 , 0x37 , 0x00},
+		{2 , 0x38 , 0x3C},    //78 FOR GPWR1/2 cycle 2 s
+		{2 , 0x39 , 0x00},
+		{2 , 0x3A , 0x00},
+		{2 , 0x3B , 0x00},
+		{2 , 0x3C , 0x00},
+		{2 , 0x3D , 0x00},
+		{2 , 0x3E , 0x00},
+		{2 , 0x3F , 0x00},
+		{2 , 0x40 , 0x00},
+		{2 , 0x41 , 0x00},
+		{2 , 0x42 , 0x00},
+		{2 , 0x43 , 0x00},
+		{2 , 0x44 , 0x00},
+		
+		//GIP_2
+		{2 , 0x50 , 0x10},
+		{2 , 0x51 , 0x32},
+		{2 , 0x52 , 0x54},
+		{2 , 0x53 , 0x76},
+		{2 , 0x54 , 0x98},
+		{2 , 0x55 , 0xba},
+		{2 , 0x56 , 0x10},
+		{2 , 0x57 , 0x32},
+		{2 , 0x58 , 0x54},
+		{2 , 0x59 , 0x76},
+		{2 , 0x5A , 0x98},
+		{2 , 0x5B , 0xba},
+		{2 , 0x5C , 0xdc},
+		{2 , 0x5D , 0xfe},
+		
+		//GIP_3
+		{2 , 0x5E , 0x00},
+		{2 , 0x5F , 0x01},     //GOUT1_FW
+		{2 , 0x60 , 0x00},     //GOUT2_BW
+		{2 , 0x61 , 0x15},     //GOUT3_GPWR1
+		{2 , 0x62 , 0x14},     //GOUT4_GPWR2
+		{2 , 0x63 , 0x0E},     //GOUT5_CLK1_R
+		{2 , 0x64 , 0x0F},     //GOUT6_CLK2_R
+		{2 , 0x65 , 0x0C},     //GOUT7_CLK3_R
+		{2 , 0x66 , 0x0D},     //GOUT8_CLK4_R
+		{2 , 0x67 , 0x06},     //GOUT9_STV1_R
+		{2 , 0x68 , 0x02},
+		{2 , 0x69 , 0x02},
+		{2 , 0x6A , 0x02},
+		{2 , 0x6B , 0x02},
+		{2 , 0x6C , 0x02},
+		{2 , 0x6D , 0x02},
+		{2 , 0x6E , 0x07},      //GOUT16_STV2_R
+		{2 , 0x6F , 0x02},      //GOUT17_VGL
+		{2 , 0x70 , 0x02},      //GOUT18_VGL
+		{2 , 0x71 , 0x02},      //GOUT19_VGL
+		{2 , 0x72 , 0x02},      
+		{2 , 0x73 , 0x02},      
+		{2 , 0x74 , 0x02},      
+		{2 , 0x75 , 0x01},      //FW
+		{2 , 0x76 , 0x00},      //BW
+		{2 , 0x77 , 0x14},      //GPWR1
+		{2 , 0x78 , 0x15},      //GPWR2
+		{2 , 0x79 , 0x0E},      //CLK1_R
+		{2 , 0x7A , 0x0F},      //CLK2_R
+		{2 , 0x7B , 0x0C},      //CLK3_R
+		{2 , 0x7C , 0x0D},      //CLK4_R
+		{2 , 0x7D , 0x06},      //STV1_R
+		{2 , 0x7E , 0x02},      
+		{2 , 0x7F , 0x02},     
+		{2 , 0x80 , 0x02},     
+		{2 , 0x81 , 0x02},      
+		{2 , 0x82 , 0x02},      
+		{2 , 0x83 , 0x02},      
+		{2 , 0x84 , 0x07},      //STV2_R 
+		{2 , 0x85 , 0x02},      //VGL
+		{2 , 0x86 , 0x02},      //VGL
+		{2 , 0x87 , 0x02},      //VGL 
+		{2 , 0x88 , 0x02},       
+		{2 , 0x89 , 0x02},      
+		{2 , 0x8A , 0x02},       
+		
+		//CMD_Page 4
+		{4 , 0xFF , 0x98 , 0x81 , 0x04},
+		{2 , 0x6C , 0x15},       //Set VCORE voltage =1.5V
+		{2 , 0x6E , 0x2A},       //di_pwr_reg=0 for power mode 2A //VGH clamp 15V
+		{2 , 0x6F , 0x35},       // reg vcl + pumping ratio VGH=3x VGL=-2.5x
+		{2 , 0x3A , 0x24},       //POWER SAVING
+		{2 , 0x8D , 0x1F},       //VGL clamp -11V
+		{2 , 0x87 , 0xBA},       //ESD   
+		{2 , 0x26 , 0x76},           
+		{2 , 0xB2 , 0xD1},       
+		{2 , 0xB5 , 0x27},       //GMA BIAS 
+		{2 , 0x31 , 0x75},       //SRC BIAS 
+		{2 , 0x30 , 0x03},       //SRC OUTPUT BIAS 
+		{2 , 0x3B , 0x98},       //PUMP SHIFT CLK 
+		{2 , 0x35 , 0x1f},       //HZ_opt 
+		{2 , 0x33 , 0x14},       //Blanking frame ÔO¶¨žéGND 
+		{2 , 0x7A , 0x0F},
+		{2 , 0x38 , 0x02},
+		{2 , 0x39 , 0x00},
+		
+		//CMD_Page 1
+		{4 , 0xFF , 0x98 , 0x81 , 0x01},
+		{2 , 0x22 , 0x0A},	//BGR, SS
+		{2 , 0x31 , 0x00},//column inversion
+		{2 , 0x53 , 0x35},	//VCOM1
+		{2 , 0x55 , 0x69},	//VCOM2
+		{2 , 0x50 , 0xC7},	//VREG1OUT=5V
+		{2 , 0x51 , 0xC2},	//VREG2OUT=-5V
+		{2 , 0x60 , 0x26},      //SDT
+		{2 , 0x63 , 0x00}, 
+		{2 , 0xA0 , 0x08},	//VP255	Gamma P 
+		{2 , 0xA1 , 0x0F},      //VP251 
+		{2 , 0xA2 , 0x25},      //VP247   
+		{2 , 0xA3 , 0x01},      //VP243  
+		{2 , 0xA4 , 0x23},      //VP239  
+		{2 , 0xA5 , 0x18},      //VP231   
+		{2 , 0xA6 , 0x11},      //VP219   
+		{2 , 0xA7 , 0x1A},      //VP203  
+		{2 , 0xA8 , 0x81},      //VP175 
+		{2 , 0xA9 , 0x19},      //VP144   
+		{2 , 0xAA , 0x26},      //VP111   
+		{2 , 0xAB , 0x7C},      //VP80     
+		{2 , 0xAC , 0x24},      //VP52   
+		{2 , 0xAD , 0x1E},      //VP36  
+		{2 , 0xAE , 0x5C},      //VP24     
+		{2 , 0xAF , 0x2A},      //VP16    
+		{2 , 0xB0 , 0x2B},      //VP12      
+		{2 , 0xB1 , 0x50},      //VP8  
+		{2 , 0xB2 , 0x5C},      //VP4   
+		{2 , 0xB3 , 0x39},      //VP0   
+		{2 , 0xC0 , 0x08},	//VN255 GAMMA N 
+		{2 , 0xC1 , 0x1F},      //VN251 
+		{2 , 0xC2 , 0x24},      //VN247   
+		{2 , 0xC3 , 0x1D},      //VN243    
+		{2 , 0xC4 , 0x04},      //VN239 
+		{2 , 0xC5 , 0x32},      //VN231  
+		{2 , 0xC6 , 0x24},      //VN219    
+		{2 , 0xC7 , 0x1F},      //VN203  
+		{2 , 0xC8 , 0x90},      //VN175 
+		{2 , 0xC9 , 0x20},      //VN144   
+		{2 , 0xCA , 0x2C},      //VN111  
+		{2 , 0xCB , 0x82},      //VN80 
+		{2 , 0xCC , 0x19},      //VN52 
+		{2 , 0xCD , 0x22},      //VN36  
+		{2 , 0xCE , 0x4E},      //VN24   
+		{2 , 0xCF , 0x28},      //VN16     
+		{2 , 0xD0 , 0x2D},      //VN12   
+		{2 , 0xD1 , 0x51},      //VN8 
+		{2 , 0xD2 , 0x5D},      //VN4       
+		{2 , 0xD3 , 0x39},      //VN0 
+		
+		//CMD_Page 0
+		{4 , 0xFF , 0x98 , 0x81 , 0x00},
+	};
+#endif /* CONFIG_DRM_PANEL_AVDTT80WXCN039A */
+
+#ifdef CONFIG_DRM_PANEL_AVDTT80WXCN056A
+	/* Datasheet: AVD-TT80WX-CN-056-A.pdf */
+	static u8 d[196][8] = {
+		{4 ,0xFF ,98 ,81 ,03},
+		
+		//GIP_1
+		{2 ,0x01 ,0x00}, 
+		{2 ,0x02 ,0x00}, 
+		{2 ,0x03 ,0x73},    //STA Width 4H 
+		{2 ,0x04 ,0x00}, 
+		{2 ,0x05 ,0x00}, 
+		{2 ,0x06 ,0x0A},    //STVA Rise start 
+		{2 ,0x07 ,0x00}, 
+		{2 ,0x08 ,0x00}, 
+		{2 ,0x09 ,0x20},    //Detail A&B 3.5-4.0us 
+		{2 ,0x0A ,0x20},    //Detail A&B 3.5-4.0us 
+		{2 ,0x0B ,0x00}, 
+		{2 ,0x0C ,0x00}, 
+		{2 ,0x0D ,0x00}, 
+		{2 ,0x0E ,0x00}, 
+		{2 ,0x0F ,0x1E},     //Detail A&B 3.5-4.0us 
+		{2 ,0x10 ,0x1E},     //Detail A&B 3.5-4.0us 
+		{2 ,0x11 ,0x00}, 
+		{2 ,0x12 ,0x00}, 
+		{2 ,0x13 ,0x00}, 
+		{2 ,0x14 ,0x00}, 
+		{2 ,0x15 ,0x10},     ///////////0426 
+		{2 ,0x16 ,0x10},     ////////0426 
+		{2 ,0x17 ,0x03}, 
+		{2 ,0x18 ,0x03}, 
+		{2 ,0x19 ,0x00}, 
+		{2 ,0x1A ,0x00}, 
+		{2 ,0x1B ,0x00}, 
+		{2 ,0x1C ,0x00}, 
+		{2 ,0x1D ,0x00}, 
+		{2 ,0x1E ,0x40}, 
+		{2 ,0x1F ,0x80}, 
+		{2 ,0x20 ,0x06},    //CLKA Rise START 
+		{2 ,0x21 ,0x01},    //CLKA FALL END 
+		{2 ,0x22 ,0x00}, 
+		{2 ,0x23 ,0x00}, 
+		{2 ,0x24 ,0x00}, 
+		{2 ,0x25 ,0x00}, 
+		{2 ,0x26 ,0x00}, 
+		{2 ,0x27 ,0x00}, 
+		{2 ,0x28 ,0x33},    //CLK_x_Numb[2:0]  Phase_CLK[2:0] 
+		{2 ,0x29 ,0x03},    //Overlap_CLK[3:0] 
+		{2 ,0x2A ,0x00}, 
+		{2 ,0x2B ,0x00}, 
+		{2 ,0x2C ,0x00}, 
+		{2 ,0x2D ,0x00}, 
+		{2 ,0x2E ,0x00}, 
+		{2 ,0x2F ,0x00}, 
+		{2 ,0x30 ,0x00}, 
+		{2 ,0x31 ,0x00}, 
+		{2 ,0x32 ,0x00}, 
+		{2 ,0x33 ,0x00}, 
+		{2 ,0x34 ,0x04},     //04 GPWR1/2 non overlap time 2.62us 
+		{2 ,0x35 ,0x00}, 
+		{2 ,0x36 ,0x00}, 
+		{2 ,0x37 ,0x00}, 
+		{2 ,0x38 ,0x3C},     //78 FOR GPWR1/2 cycle 2 s 
+		{2 ,0x39 ,0x00}, 
+		{2 ,0x3A ,0x00}, 
+		{2 ,0x3B ,0x00}, 
+		{2 ,0x3C ,0x00}, 
+		{2 ,0x3D ,0x00}, 
+		{2 ,0x3E ,0x00}, 
+		{2 ,0x3F ,0x00}, 
+		{2 ,0x40 ,0x00}, 
+		{2 ,0x41 ,0x00}, 
+		{2 ,0x42 ,0x00}, 
+		{2 ,0x43 ,0x00}, 
+		{2 ,0x44 ,0x00}, 
+		
+		//GIP_2
+		{2 ,0x50 ,0x10}, 
+		{2 ,0x51 ,0x32}, 
+		{2 ,0x52 ,0x54}, 
+		{2 ,0x53 ,0x76}, 
+		{2 ,0x54 ,0x98}, 
+		{2 ,0x55 ,0xba}, 
+		{2 ,0x56 ,0x10}, 
+		{2 ,0x57 ,0x32}, 
+		{2 ,0x58 ,0x54}, 
+		{2 ,0x59 ,0x76}, 
+		{2 ,0x5A ,0x98}, 
+		{2 ,0x5B ,0xba}, 
+		{2 ,0x5C ,0xdc}, 
+		{2 ,0x5D ,0xfe},
+		
+		//GIP_3
+		{2 ,0x5E ,0x00}, 
+		{2 ,0x5F ,0x01},      //GOUT1_FW 
+		{2 ,0x60 ,0x00},      //GOUT2_BW 
+		{2 ,0x61 ,0x15},      //GOUT3_GPWR1 
+		{2 ,0x62 ,0x14},      //GOUT4_GPWR2 
+		{2 ,0x63 ,0x0E},      //GOUT5_CLK1_R 
+		{2 ,0x64 ,0x0F},      //GOUT6_CLK2_R 
+		{2 ,0x65 ,0x0C},      //GOUT7_CLK3_R 
+		{2 ,0x66 ,0x0D},      //GOUT8_CLK4_R 
+		{2 ,0x67 ,0x06},      //GOUT9_STV1_R 
+		{2 ,0x68 ,0x02}, 
+		{2 ,0x69 ,0x02}, 
+		{2 ,0x6A ,0x02}, 
+		{2 ,0x6B ,0x02}, 
+		{2 ,0x6C ,0x02}, 
+		{2 ,0x6D ,0x02}, 
+		{2 ,0x6E ,0x07},       //GOUT16_STV2_R 
+		{2 ,0x6F ,0x02},       //GOUT17_VGL 
+		{2 ,0x70 ,0x02},       //GOUT18_VGL 
+		{2 ,0x71 ,0x02},       //GOUT19_VGL 
+		{2 ,0x72 ,0x02},       
+		{2 ,0x73 ,0x02},       
+		{2 ,0x74 ,0x02},       
+		{2 ,0x75 ,0x01},       //FW 
+		{2 ,0x76 ,0x00},       //BW 
+		{2 ,0x77 ,0x14},       //GPWR1 
+		{2 ,0x78 ,0x15},       //GPWR2 
+		{2 ,0x79 ,0x0E},       //CLK1_R 
+		{2 ,0x7A ,0x0F},       //CLK2_R 
+		{2 ,0x7B ,0x0C},       //CLK3_R 
+		{2 ,0x7C ,0x0D},       //CLK4_R 
+		{2 ,0x7D ,0x06},       //STV1_R 
+		{2 ,0x7E ,0x02},       
+		{2 ,0x7F ,0x02},      
+		{2 ,0x80 ,0x02},      
+		{2 ,0x81 ,0x02},       
+		{2 ,0x82 ,0x02},       
+		{2 ,0x83 ,0x02},       
+		{2 ,0x84 ,0x07},       //STV2_R 
+		{2 ,0x85 ,0x02},       //VGL 
+		{2 ,0x86 ,0x02},       //VGL 
+		{2 ,0x87 ,0x02},       //VGL 
+		{2 ,0x88 ,0x02},        
+		{2 ,0x89 ,0x02},       
+		{2 ,0x8A ,0x02},        
+		
+		//CMD_Page 4
+		{4 ,0xFF ,98 ,81 ,04},
+		
+		{2 ,0x6C ,0x15},        //Set VCORE voltage =1.5V 
+		{2 ,0x6E ,0x2A},        //di_pwr_reg=0 for power mode 2A //VGH clamp 15V 
+		{2 ,0x6F ,0x35},        // reg vcl + pumping ratio VGH=3x VGL=-2.5x 
+		{2 ,0x3A ,0x24},        //POWER SAVING 
+		{2 ,0x8D ,0x1F},        //VGL clamp -11V 
+		{2 ,0x87 ,0xBA},        //ESD   
+		{2 ,0x26 ,0x76},            
+		{2 ,0xB2 ,0xD1},        
+		{2 ,0xB5 ,0x27},        //GMA BIAS 
+		{2 ,0x31 ,0x75},        //SRC BIAS 
+		{2 ,0x30 ,0x03},        //SRC OUTPUT BIAS 
+		{2 ,0x3B ,0x98},        //PUMP SHIFT CLK 
+		{2 ,0x35 ,0x1f},        //HZ_opt 
+		{2 ,0x33 ,0x14},        //Blanking frame ?O?¡§?¨¦GND 
+		{2 ,0x7A ,0x0F}, 
+		{2 ,0x38 ,0x02}, 
+		{2 ,0x39 ,0x00}, 
+		
+		//CMD_Page 1
+		{4 ,0xFF ,98 ,81 ,01},
+		
+		{2 ,0x22 ,0x0A},	//BGR, SS
+		{2 ,0x31 ,0x00},	//column inversion 
+		{2 ,0x53 ,0x43},	//VCOM1 
+		{2 ,0x55 ,0x43},	//VCOM2
+		{2 ,0x50 ,0xA7},	//VREG1OUT=5V 
+		{2 ,0x51 ,0xA3},	//VREG2OUT=-5V 
+		{2 ,0x60 ,0x26},        //SDT 
+		{2 ,0x63 ,0x00},  
+		{2 ,0xA0 ,0x08},	//VP255	Gamma P 
+		{2 ,0xA1 ,0x0A},        //VP251 
+		{2 ,0xA2 ,0x1D},        //VP247   
+		{2 ,0xA3 ,0x02},        //VP243  
+		{2 ,0xA4 ,0x20},        //VP239  
+		{2 ,0xA5 ,0x14},        //VP231   
+		{2 ,0xA6 ,0x0D},        //VP219   
+		{2 ,0xA7 ,0x18},        //VP203  
+		{2 ,0xA8 ,0x67},        //VP175 
+		{2 ,0xA9 ,0x19},        //VP144   
+		{2 ,0xAA ,0x25},        //VP111   
+		{2 ,0xAB ,0x68},        //VP80     
+		{2 ,0xAC ,0x25},        //VP52   
+		{2 ,0xAD ,0x1F},        //VP36  
+		{2 ,0xAE ,0x5D},        //VP24     
+		{2 ,0xAF ,0x29},        //VP16    
+		{2 ,0xB0 ,0x2B},        //VP12      
+		{2 ,0xB1 ,0x4B},        //VP8  
+		{2 ,0xB2 ,0x57},        //VP4   
+		{2 ,0xB3 ,0x39},        //VP0   
+		{2 ,0xC0 ,0x08},	//VN255 GAMMA N 
+		{2 ,0xC1 ,0x1B},        //VN251        
+		{2 ,0xC2 ,0x1C},        //VN247   
+		{2 ,0xC3 ,0x1D},        //VN243    
+		{2 ,0xC4 ,0x02},        //VN239 
+		{2 ,0xC5 ,0x2E},        //VN231  
+		{2 ,0xC6 ,0x20},        //VN219    
+		{2 ,0xC7 ,0x1D},        //VN203  
+		{2 ,0xC8 ,0x76},        //VN175 
+		{2 ,0xC9 ,0x20},        //VN144   
+		{2 ,0xCA ,0x2B},        //VN111  
+		{2 ,0xCB ,0x6E},        //VN80 
+		{2 ,0xCC ,0x1A},        //VN52
+		{2 ,0xCD ,0x23},        //VN36  
+		{2 ,0xCE ,0x4F},        //VN24   
+		{2 ,0xCF ,0x28},        //VN16     
+		{2 ,0xD0 ,0x2E},        //VN12   
+		{2 ,0xD1 ,0x4D},        //VN8 
+		{2 ,0xD2 ,0x59},        //VN4       
+		{2 ,0xD3 ,0x39},        //VN0 
+		
+		//CMD_Page 0
+		{4 ,0xFF ,98 ,81 ,00},
+	};
+
+#endif /* CONFIG_DRM_PANEL_AVDTT80WXCN056A */
+
 	int i;
 	u8 *pd;
 
@@ -370,6 +826,7 @@ static void _set_sequence(struct avdtt80wx *ctx)
 	avdtt80wx_dcs_write_seq(ctx, SLPIN);
 	msleep(150);
 
+#ifdef CONFIG_DRM_PANEL_AVDTT80WXCN046A
 	for (i = 0; i < 193; ++i)
 	{
 		pd = &d[i][0];
@@ -379,6 +836,37 @@ static void _set_sequence(struct avdtt80wx *ctx)
 	avdtt80wx_dcs_write_seq(ctx, SLPOUT);
 	msleep(150);
 	avdtt80wx_dcs_write_seq(ctx, DISPON);
+	msleep(150);
+#endif /* CONFIG_DRM_PANEL_AVDTT80WXCN046A */
+
+#ifdef CONFIG_DRM_PANEL_AVDTT80WXCN039A
+	for (i = 0; i < 196; ++i)
+	{
+		pd = &d[i][0];
+		_dcs_write(ctx, pd + 1, pd[0]);
+	}
+
+	avdtt80wx_dcs_write_seq(ctx, SLPOUT);
+	msleep(120);
+	avdtt80wx_dcs_write_seq(ctx, DISPON);
+	msleep(20);
+	avdtt80wx_dcs_write_seq(ctx, TEON);
+#endif /* CONFIG_DRM_PANEL_AVDTT80WXCN039A */
+
+#ifdef CONFIG_DRM_PANEL_AVDTT80WXCN056A
+	for (i = 0; i < 196; ++i)
+	{
+		pd = &d[i][0];
+		_dcs_write(ctx, pd + 1, pd[0]);
+	}
+
+	avdtt80wx_dcs_write_seq(ctx, SLPOUT);
+	msleep(120);
+	avdtt80wx_dcs_write_seq(ctx, DISPON);
+	msleep(20);
+	avdtt80wx_dcs_write_seq(ctx, TEON);
+#endif /* CONFIG_DRM_PANEL_AVDTT80WXCN056A */
+
 }
 
 #endif
@@ -414,6 +902,10 @@ static int avdtt80wx_power_off(struct avdtt80wx *ctx)
 	if (!ctx->is_power_on)
 		return 0;
 
+	avdtt80wx_dcs_write_seq(ctx, DISPOFF);
+	avdtt80wx_dcs_write_seq(ctx, SLPIN);
+	msleep(150);
+
 /*
 	gpio_set_value(ctx->reset_gpio, 0);
 	usleep_range(5000, 6000);
@@ -423,8 +915,6 @@ static int avdtt80wx_power_off(struct avdtt80wx *ctx)
 		ctx->bl_dev->props.power = FB_BLANK_POWERDOWN;
 		backlight_update_status(ctx->bl_dev);
 	}
-
-	avdtt80wx_dcs_write_seq(ctx, DISPOFF);
 
 	ctx->is_power_on = false;
 
@@ -441,7 +931,11 @@ static int avdtt80wx_enable(struct drm_panel *panel)
 		backlight_update_status(ctx->bl_dev);
 	}
 
+	avdtt80wx_dcs_write_seq(ctx, SLPOUT);
+	msleep(120);
 	avdtt80wx_dcs_write_seq(ctx, DISPON);
+	msleep(20);
+	avdtt80wx_dcs_write_seq(ctx, TEON);
 
 	return 0;
 }
